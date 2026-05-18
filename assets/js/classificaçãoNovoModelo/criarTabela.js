0fc5b5641e9dc3dfaf77980c9bef4fca61925c19
Brenno Thiago <167652253+Brennoth@users.noreply.github.com>
2026-05-17 21:41:27 -0300

    ajuste

--- assets/js/classificaçãoNovoModelo/criarTabela.js
+++ assets/js/classificaçãoNovoModelo/criarTabela.js
@@ -1,1 +1,36 @@
-console.log("ola")
+let jogadores 
+let equipes 
+
+async function carregarDados() {
+    const resposta = await fetch("../../../assets/data/xtreinoMensal/equipe.json")
+    equipes = await resposta.json()
+
+
+    const resposta2 = await fetch("../../../assets/data/xtreinoMensal/jogadores.json")
+    jogadores = await resposta2.json()
+}
+//o js para e espera pegar todos os dados no json
+async function iniciarApp() {
+    await carregarDados()
+    organizarDados.start(equipes, jogadores)
+}
+iniciarApp()
+
+const organizarDados = {
+
+    //aq esta selecionando o mes do json
+    start(equipes, jogadores) {
+        //pega o mes do html selecionado
+        const mesDoHtmlSelecionado = document.querySelector("#mes").textContent.toLowerCase()
+
+        //vai percorrer todos os meses no json
+        for (let mesEquipes in equipes) {
+
+            //com o mes selecionado no html e o mes percorrido, tem que ser o mesmo
+            if (mesEquipes === mesDoHtmlSelecionado) {
+                
+            }
+
+        }
+    }
+}
